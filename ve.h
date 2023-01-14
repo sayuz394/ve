2104a233e1859620687d9c4e192bdf168e0126c7
sayuz394 <71702420+sayuz394@users.noreply.github.com>
2023-01-14 12:18:17 +0000

    Add files via upload

---
+++ ve.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define tulis cout
+#define ambil cin
+#define a <<
+#define b >>
+#define jika if
+#define var int
+#define jika if
+#define sebaliknya else
+#define huruf string
+
+void Bawah(){
+	cout << "\n";
+}
+
